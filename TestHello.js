edb7d49de9426ddf014cbf9328890fea1b13a3f3
bouckboyd <bbouck@uw.edu>
2024-10-24 22:19:43 -0700

    Update TestHello.js from GitHub again

--- TestHello.js
+++ TestHello.js
@@ -2,3 +2,4 @@ console.log("Hello World!");
 console.log("testing push to GitHub from VS code file");
 console.log("now attempting to use git directly from vscode");
 console.log("this edit was made from github to test pull directly through vscode");
+console.log("another edit from GitHub");
