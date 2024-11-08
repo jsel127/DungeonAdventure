4fc927bf91732746403026124ed5abb08e63f077
Boyd Bouck <bbouck@uw.edu>
2024-11-07 20:10:32 -0800

    moved test files to __tests__ folder

--- HealingPotion.test.js
+++
@@ -1,5 +0,0 @@
-const HealingPotion = require('./HealingPotion');
-
-test('constructor with no arguments', () => {
-    expect(new HealingPotion().getName()).toBe('Healing Potion');
-});
--- VisionPotion.test.js
+++
@@ -1,5 +0,0 @@
-const VisionPotion = require('./VisionPotion');
-
-test('constructor with no arguments', () => {
-    expect(new VisionPotion().getName()).toBe('Vision Potion');
-});
