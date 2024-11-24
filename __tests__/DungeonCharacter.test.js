3ea181e28ce625b0dfa04bb0062aae572fa78a2c
Jasmine Sellers <jsel4635@gmail.com>
2024-11-24 15:45:10 -0800

    Fixed MonsterFactory and HeroFactory tests to change made from instance method to static method. Fixed DungeonCharacter test in response to implementation of DungeonCharacter to an abstract class. Created a Hero test to ensure it is also stimulated as an abstract class.

--- __tests__/DungeonCharacter.test.js
+++ __tests__/DungeonCharacter.test.js
@@ -4,72 +4,8 @@
  * Jasmine Sellers, Boyd Bouck, Simran Narwal
  */
 import DungeonCharacter from "../characters/DungeonCharacter.js";
-// TODO: test attack method
-describe('Tests Dungeon Character instanciated to Name: Alice, HP: 20, DPMin: 1, DPMax: 5, AttackSpeed: 1, HitChange: 100', () => {
-    const oneHundredHitChance = new DungeonCharacter("Alice", 20, 1, 5, 1, 100);  
-    test('Creating the character', () => {
-        expect(oneHundredHitChance.toString()).toBe("Alice 20 1 5 1 100");
+describe('Tests simulation of abstract class DungeonCharacter.', () => {
+    test('Instanciates a DungeonCharacter object. Should throw an error', () => {
+        expect(() => new DungeonCharacter("Incorrect", 125, 35, 60, 4, 80)).toThrow(TypeError);
     });
-
-    // GETTER methods
-    test('getName', () => {
-        expect(oneHundredHitChance.getName()).toBe("Alice");
-    });
-
-    test('getHP', () => {
-        expect(oneHundredHitChance.getHP()).toBe(20);
-    });
-
-    test('getDPMin', () => {
-        expect(oneHundredHitChance.getDPMin()).toBe(1);
-    });
-
-    test('getDPMax', () => {
-        expect(oneHundredHitChance.getDPMax()).toBe(5);
-    });
-
-    test('getAttackSpeed', () => {
-        expect(oneHundredHitChance.getAttackSpeed()).toBe(1);
-    });
-
-    test('getHitChance', () => {
-        expect(oneHundredHitChance.getHitChance()).toBe(100);
-    });
-
-    // SETTER methods
-    test('setHP to smaller number (10)', () => {
-        oneHundredHitChance.setHP(10);
-        expect(oneHundredHitChance.getHP()).toBe(10);
-    });
-
-    test('setHP to larger number (500)', () => {
-        oneHundredHitChance.setHP(500);
-        expect(oneHundredHitChance.getHP()).toBe(500);
-    });
-
-    test('setHP to 0', () => {
-        oneHundredHitChance.setHP(0);
-        expect(oneHundredHitChance.getHP()).toBe(0);
-    });
-
-    test('setHP to -1', () => {
-        expect(() => oneHundredHitChance.setHP(-1)).toThrow(RangeError);
-    });
-
-    test('isDead when the character is NOT dead HP 100', () => {
-        oneHundredHitChance.setHP(100);
-        expect(oneHundredHitChance.isDead()).toBeFalsy();
-    });
-
-    test('isDead when the character is NOT dead HP 1', () => {
-        oneHundredHitChance.setHP(1);
-        expect(oneHundredHitChance.isDead()).toBeFalsy();
-    });
-
-    test('isDead when the character is dead', () => {
-        oneHundredHitChance.setHP(0);
-        expect(oneHundredHitChance.isDead()).toBeTruthy();
-    });
-})
-
-
+});
--- __tests__/HeroFactory.test.js
+++ __tests__/HeroFactory.test.js
@@ -10,20 +10,18 @@ import Priestess from "../characters/Priestess";
 import HeroFactory from "../characters/HeroFactory";
 
 describe("Testing hero factory to ensure proper hero is created and the object is instaciated.", () => {
-    const heroFactory = new HeroFactory();
-
     test("Creating a warrior", () => {
-        const warrior = heroFactory.createHero("Warrior", 125, 35, 60, 4, 80, 20);
+        const warrior = HeroFactory.createHero("Warrior", 125, 35, 60, 4, 80, 20);
         expect(warrior instanceof Warrior).toBeTruthy();
         expect(warrior.toString()).toBe("Warrior 125 35 60 4 80 20");
     });
     test("Creating a priestess", () => {
-        const priestess = heroFactory.createHero("Priestess", 75, 25, 45, 5, 70, 30);
+        const priestess = HeroFactory.createHero("Priestess", 75, 25, 45, 5, 70, 30);
         expect(priestess instanceof Priestess).toBeTruthy();
         expect(priestess.toString()).toBe("Priestess 75 25 45 5 70 30");
     });
     test("Creating a thief", () => {
-        const thief = heroFactory.createHero("Thief", 75, 20, 40, 6, 80, 40);
+        const thief = HeroFactory.createHero("Thief", 75, 20, 40, 6, 80, 40);
         expect(thief instanceof Thief).toBeTruthy();
         expect(thief.toString()).toBe("Thief 75 20 40 6 80 40")
     });
