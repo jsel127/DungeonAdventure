8773d68652962376fac37895aad6cebacbd81c94
Jasmine Sellers <jsel4635@gmail.com>
2024-11-26 09:41:14 -0800

    Added getHeroes method that returns an array of objects with the heroes' stats.

--- DungeonAdventure.js
+++ DungeonAdventure.js
@@ -14,6 +14,38 @@ export default class DungeonAdventure {
         return "Welcome to the Dungeon Adventure Game. There are three major heroes in the worlds of Dungeon Adventure, the Warrior, the Priestess, and the Thief. Your goal is to find the missing pillars of OOP which are currently somewhere in the dungeon. Once you find all pillars find the exit to save the world of OOP."
     }
 
+    getHeroes() {
+        return [
+            {
+                name: "Warrior",
+                hp: 125,
+                dpmin: 35,
+                dpmax: 60,
+                attack_speed: 4,
+                hit_chance: 80,
+                block_chance: 20
+            },
+            {
+                name: "Priestess",
+                hp: 75,
+                dpmin: 25,
+                dpmax: 45,
+                attack_speed: 5,
+                hit_chance: 70,
+                block_chance: 30
+            },
+            {
+                name: "Thief",
+                hp: 75,
+                dpmin: 20,
+                dpmax: 40,
+                attack_speed: 6,
+                hit_chance: 80,
+                block_chance: 40
+            }
+        ];
+    }
+
     setAdventurer(theName, theHeroType) {
         if (typeof theName !== "string") {
             throw new TypeError("Invalid name provided");
@@ -87,6 +119,11 @@ export default class DungeonAdventure {
     #processMove() {
         const content = this.#myCurrentRoom.getContent();
     }
+
+    #pickupItem() {
+
+    }
+
     #fightMonster(theMonster) {
         // const adventurerHeroChar = this.#myAdventurer.getHero();
         // let time = 0;
