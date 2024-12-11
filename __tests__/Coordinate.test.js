ba8315a45952800c1f83b53b8e6ced64b37bce5c
Jasmine Sellers <jsel4635@gmail.com>
2024-12-10 23:10:06 -0800

    Created more tests for the Coordinate class. Specifically for save and load and range of valid data (buffer).

--- __tests__/Coordinate.test.js
+++ __tests__/Coordinate.test.js
@@ -4,7 +4,13 @@
  * Jasmine Sellers, Boyd Bouck, Simran Narwal
  */
 import Coordinate from "../dungeon/Coordinate.js";
+import Dungeon from "../dungeon/Dungeon.js";
 
+/**
+ * Test Coordinate class.
+ * @author Jasmine Sellers
+ * @version 1.0
+ */
 describe("Basic behavior of a coordinate no invalid input", () => {
     const coord = new Coordinate(1, 2);
     test("Creating a coordinate", () => {
@@ -25,10 +31,10 @@ describe("Basic behavior of a coordinate no invalid input", () => {
 
 describe("Invalid input constructor and setter methods", () => {
     test("String x", () => {
-        expect(() => new Coordinate("invalid", 1)).toThrow();
+        expect(() => new Coordinate("invalid", 1)).toThrow(TypeError);
     });
     test("String y", () => {
-        expect(() => new Coordinate(1, "invalid")).toThrow();
+        expect(() => new Coordinate(1, "invalid")).toThrow(TypeError);
     });
     test("Negative x", () => {
         expect(() => new Coordinate(-1, 1)).toThrow(RangeError);
@@ -36,6 +42,14 @@ describe("Invalid input constructor and setter methods", () => {
     test("Negative y", () => {
         expect(() => new Coordinate(1, -1)).toThrow(RangeError);
     });
+
+    test("less than buffer x", () => {
+        expect(() => new Coordinate(Dungeon.BUFFER - 1, 1)).toThrow(RangeError);
+    });
+    test("less than buffer y", () => {
+        expect(() => new Coordinate(1, Dungeon.BUFFER - 1)).toThrow(RangeError);
+    });
+
     test("Setting valid coordinate to a negative x", () => {
         expect(() => {
             const coord = new Coordinate(1, 2);
@@ -54,7 +68,21 @@ describe("Tests Saves and Loads Coordinate class", () => {
     test("Saves and Loads Coordinate class properly (No chances made from initialization)", () => {
         const coordinateToSave = new Coordinate(10, 11);  
         const coordinateFromSave = Coordinate.fromJSON(JSON.parse(JSON.stringify(coordinateToSave)));
-        expect(coordinateFromSave.toString()).toBe(coordinateToSave.toString());
+        expect(JSON.stringify(coordinateFromSave)).toBe(JSON.stringify(coordinateToSave));
+    });
+
+    test("Saves and Loads Coordinate class properly (col changed)", () => {
+        const coordinateToSave = new Coordinate(10, 11);  
+        coordinateToSave.setCol(1);
+        const coordinateFromSave = Coordinate.fromJSON(JSON.parse(JSON.stringify(coordinateToSave)));
+        expect(JSON.stringify(coordinateFromSave)).toBe(JSON.stringify(coordinateToSave));
+    });
+
+    test("Saves and Loads Coordinate class properly (row changed)", () => {
+        const coordinateToSave = new Coordinate(10, 11);  
+        coordinateToSave.setRow(19);
+        const coordinateFromSave = Coordinate.fromJSON(JSON.parse(JSON.stringify(coordinateToSave)));
+        expect(JSON.stringify(coordinateFromSave)).toBe(JSON.stringify(coordinateToSave));
     });
 
     test("Save and Load on invalid data", () => {
